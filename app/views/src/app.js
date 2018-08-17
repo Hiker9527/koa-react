c9439b078ae8370c46ccb99c37c8ed9e3b3a3297
wangxiaoxiong <xiaoxiong.wang@bianlifeng.com>
2018-08-17 11:13:45 +0800

    ignore

--- app/views/src/app.js
+++ app/views/src/app.js
@@ -8,8 +8,10 @@ import {
 
 import { StyleRoot } from 'radium';
 import { connect } from 'react-redux';
+// 这一部分应该放在一个 views 得文件夹下
 import Index from './component/Index';
 import AccessArticle from './container/AccessArticle';
+import NoMatch from './component/NoMatch';
 import './css/common.css';
 
 // auth 处理需要登录的路由
@@ -32,4 +34,46 @@ const UserIndex = ({ match }) => {
     path={`${match.url}/:userId`}
     component={AccessArticle}
   />
+}
+
+// 处理服务器重定向问题与404
+const RedirectFromServer = ({ match }) => {
+  // deal the server redirect
+  let url = window.location.search;
+  return url.substring(1) ? <Redirect to={{
+    pathname: url.substring(1),
+    state: { from: '/' }
+  }} /> : <NoMatch />
+}
+
+class App extends Component {
+  componentdidMount() {
+    let loading = document.getElementById('loading');
+    loading.style.display = 'none';
+  }
+
+  render() {
+    let auth = this.props.login;
+    return (
+      <StyleRoot>
+        <Router>
+          <div>
+            <ProcessBars />
+            <div className="container">
+              <Header/>
+              <FlashMessage/>
+              <Switch>
+                <Route exact path="/index" component={Index}/>
+                <Route path="/login" component={Login}/>
+                <PrivateRoute path="/edit/article"
+                  component={EditArticle}
+                  auth={auth}
+                />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </StyleRoot>
+    )
+  }
 }
