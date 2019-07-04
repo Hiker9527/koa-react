1dc4da0ce8d7b10060a2b950af1d01c4e3f36bbe
wxx <gfkdwang@126.com>
2019-07-05 00:32:51 +0800

    update app.js

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.statusCode(200);
+  res.status(200)
   res.set({
     'Content-Type': 'text/html',
   });
 
-  fs.createReadStream(path.join(__dirname, ''))
+  fs.createReadStream(path.join(__dirname, 'public/index.html')).pipe(res);
   
 })
+
+app.listen(4000, (err) => {
+  console.log('server start at localhost:4000')
+});
